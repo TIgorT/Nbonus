d8964f6eb1e5187bf16726e5fa51a91d6e92580c
Igor <TimoshenkoII@yandex.com>
2023-03-10 02:05:22 +0300

    Cheange commit

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        byte x = 100; // Остаток у клиентов на счету.
-        byte i = 1; // Сумма бонусов за 100 руб.
-        short y = 300; // Сумма пополнения счёта первого клиента.
-        int c = 1100; // Сумма пополнения счёта второго клиента.
-        int k;
-        if (y > 1_000) {
-            k = y / x * i;
-        } else {
-            k = 0;
-        }
+        byte remains = 100; // Остаток у клиентов на счету.
+        byte bonusRubles = 1; // Сумма бонусов за 100 руб.
+        int replenishment = 1100; // Сумма пополнения счёта второго клиента.
         int n;
-        if (c > 1_000) {
-            n = c / x * i;
+        if (replenishment > 1_000) {
+            n = replenishment / remains * bonusRubles;
         } else {
             n = 0;
         }
-        System.out.println("Клиент № 1 пополнил счёт на: " + y);
-        System.out.println("Начисленно бонусных рублей клиенту № 1: " + k);
-        System.out.println("Остаток на счёту у клиента № 1: " + (y + x + k));
-        System.out.println("Клиент № 2 пополнил счёт на: " + c);
-        System.out.println("Начисленно бонусных рублей клиенту № 2: " + n);
-        System.out.println("Остаток на счёту у клиента № 2: " + (c + x + n));
+        System.out.println("Клиент пополнил счёт на: " + replenishment);
+        System.out.println("Начисленно бонусных рублей клиенту : " + n);
+        System.out.println("Остаток на счёту у клиента : " + (replenishment + remains + n));
     }
 }
